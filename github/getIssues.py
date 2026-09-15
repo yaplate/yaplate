0cfc5be190cd04e0c6d14afc6ca1dc2e163ae5c3
Ashutosh Kumar Tiwari <ashutoshkumart82@gmail.com>
2026-09-15 12:08:58 +0530

    post comments manually

---
+++ github/getIssues.py
@@ -0,0 +1,35 @@
+import os
+import json
+import requests
+from github.installation_token import InstallationToken
+
+user = os.getenv("USER")
+repo = os.getenv("REPO")
+installationToken = InstallationToken()
+
+class GETIssues:
+    def __init__(self):
+        self.issueNumber = None
+
+    def getIssueNumber(self):
+        self.issueNumber = None
+
+        token = installationToken.getInstallationToken()
+        url = f"https://api.github.com/repos/{user}/{repo}/issues"
+
+        headers = {
+            "Authorization" : f"Bearer {token}",
+            "Accept" : "application/vnd.github+json",
+        }
+
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        issue = response.json()
+
+        # print(json.dumps(issue, indent=4))
+        print(issue[0]["number"])
+
+        self.issueNumber = issue[0]["number"]
+
+        return self.issueNumber
